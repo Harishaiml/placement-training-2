1c0129d3846033e51be67e9bf747abb626d5e941
Harishaiml <147464547+Harishaiml@users.noreply.github.com>
2025-06-23 20:39:53 +0530

    Create Intersection of Two Arrays.java

---
+++ 23-06-2025/Intersection of Two Arrays.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>(), result = new HashSet<>();
+        for (int num : nums1) set1.add(num);
+        for (int num : nums2)
+            if (set1.contains(num)) result.add(num);
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+}
