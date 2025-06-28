15d9080194b4e52bc2d790b79b357315d351241e
Harishaiml <147464547+Harishaiml@users.noreply.github.com>
2025-06-28 21:03:55 +0530

    Create  Kth Largest Element in an Array.py

---
+++ 28-06-22025/Kth Largest Element in an Array.py
@@ -0,0 +1,9 @@
+import heapq
+
+def findKthLargest(nums, k):
+    heap = []
+    for num in nums:
+        heapq.heappush(heap, num)
+        if len(heap) > k:
+            heapq.heappop(heap)
+    return heap[0]
