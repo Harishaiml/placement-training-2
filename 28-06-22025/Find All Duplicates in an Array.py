1289782dd967309edf152ab33a1216218a2b622d
Harishaiml <147464547+Harishaiml@users.noreply.github.com>
2025-06-28 21:05:22 +0530

    Create Find All Duplicates in an Array.py

---
+++ 28-06-22025/Find All Duplicates in an Array.py
@@ -0,0 +1,9 @@
+def findDuplicates(nums):
+    res = []
+    for num in nums:
+        index = abs(num) - 1
+        if nums[index] < 0:
+            res.append(abs(num))
+        else:
+            nums[index] *= -1
+    return res
